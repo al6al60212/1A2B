06e16fb5298338b45ec5e9d52a7cdc04e25cc1af
Heyi <al6al60212@gmail.com>
2022-09-17 13:56:29 +0800

    Initial Commit

---
+++ 1A2B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1A2B
+//
+//  Created by 董禾翊 on 2022/9/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
